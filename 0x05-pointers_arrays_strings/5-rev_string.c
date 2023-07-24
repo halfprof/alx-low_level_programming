4c0031579040b149c2b7585216e342c6d93fefc7
Mighouar <pr.mighouar@gmail.com>
2023-07-24 14:46:36 -0700

    5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s : string
+ */
+void rev_string(char *s)
+{
+	char r = s[0];
+	int counter = 0;
+	int i;
+
+	while (s[counter] != '\0')
+	{
+		counter++;
+	}
+	for (i = 0; i < counter; i++)
+	{
+		counter--;
+		r = s[i];
+		s[i] = s[counter];
+		s[counter] = r;
+	}
+}

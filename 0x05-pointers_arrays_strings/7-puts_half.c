136f099d5e1a3e05e24eb85e9fad621af21be956
Mighouar <pr.mighouar@gmail.com>
2023-07-24 15:20:29 -0700

    7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,11 +13,11 @@ void puts_half(char *str)
 	{
 		length++;
 	}
-	n = (length / 2)
+	n = (length / 2);
 	if ((length % 2) == 1)
 		n = ((length + 1) / 2);
 
 	for (i = n; str[i] != '\0'; i++)
 		_putchar(str[i]);
-	_putchar('\n')
+	_putchar('\n');
 }
